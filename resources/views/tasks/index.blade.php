0ddced6fa72562375f6caccde8df6833bf05d3eb
tomy-m <m.tomy1210@gmail.com>
2021-06-10 07:25:39 +0000

    pagenation

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -24,7 +24,10 @@
                 @endforeach
             </tbody>
         </table>
-    @endif
+   @endif
+
+    {{-- ページネーションのリンク --}}
+    {{ $tasks->links() }}
 
     {{-- タスク作成ページへのリンク --}}
     {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
